4c30bddbde27ab32cee2ab6f2563eaec328b4a3d
haseeb-mhk <muhammadhaseeb.bcs@gmail.com>
2024-08-25 21:38:14 +0500

    fetching products to index page

--- Clientsite/index.php
+++ Clientsite/index.php
@@ -1,3 +1,20 @@
+<?php
+
+include("includes/Connection.php");
+
+
+
+$popular_products = mysqli_query($con, "SELECT * FROM products WHERE quantity >= 15 LIMIT 6");
+
+
+
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -96,40 +113,21 @@
         </div>
 
         <div class="row">
-          <div class="col-sm-6 col-lg-4 text-center item mb-4">
-            <span class="tag">Sale</span>
-            <a href="shop-single.html"> <img src="images/product_01.png" alt="Image"></a>
-            <h3 class="text-dark"><a href="shop-single.html">Bioderma</a></h3>
-            <p class="price"><del>95.00</del> &mdash; $55.00</p>
-          </div>
-          <div class="col-sm-6 col-lg-4 text-center item mb-4">
-            <a href="shop-single.html"> <img src="images/product_02.png" alt="Image"></a>
-            <h3 class="text-dark"><a href="shop-single.html">Chanca Piedra</a></h3>
-            <p class="price">$70.00</p>
-          </div>
-          <div class="col-sm-6 col-lg-4 text-center item mb-4">
-            <a href="shop-single.html"> <img src="images/product_03.png" alt="Image"></a>
-            <h3 class="text-dark"><a href="shop-single.html">Umcka Cold Care</a></h3>
-            <p class="price">$120.00</p>
-          </div>
+          <?php    
+          
+          while($row_popular = mysqli_fetch_assoc($popular_products)){
+          
+          
+          ?>
+
 
           <div class="col-sm-6 col-lg-4 text-center item mb-4">
-
-            <a href="shop-single.html"> <img src="images/product_04.png" alt="Image"></a>
-            <h3 class="text-dark"><a href="shop-single.html">Cetyl Pure</a></h3>
-            <p class="price"><del>45.00</del> &mdash; $20.00</p>
+            
+          <a href="single_product.php?PID=<?php echo $row_popular['product_id'];  ?>"> <img src="../Adminsite/assets/img/product_images/<?php echo $row_popular['image']   ?>" alt="Image" style="max-width: 200px;max-height: 200px;min-width: 200px; min-height: 200px;"></a>
+          <h3 class="text-dark"><a href="shop-single.html"><?php  echo $row_popular['name'];  ?></a></h3>
+            <p class="price">Rs. <?php  echo $row_popular['price'];  ?></p>
           </div>
-          <div class="col-sm-6 col-lg-4 text-center item mb-4">
-            <a href="shop-single.html"> <img src="images/product_05.png" alt="Image"></a>
-            <h3 class="text-dark"><a href="shop-single.html">CLA Core</a></h3>
-            <p class="price">$38.00</p>
-          </div>
-          <div class="col-sm-6 col-lg-4 text-center item mb-4">
-            <span class="tag">Sale</span>
-            <a href="shop-single.html"> <img src="images/product_06.png" alt="Image"></a>
-            <h3 class="text-dark"><a href="shop-single.html">Poo Pourri</a></h3>
-            <p class="price"><del>$89</del> &mdash; $38.00</p>
-          </div>
+        <?php }  ?>
         </div>
         <div class="row mt-5">
           <div class="col-12 text-center">
@@ -151,29 +149,25 @@
           <div class="col-md-12 block-3 products-wrap">
             <div class="nonloop-block-3 owl-carousel">
 
-              <div class="text-center item mb-4">
-                <a href="shop-single.html"> <img src="images/product_03.png" alt="Image"></a>
-                <h3 class="text-dark"><a href="shop-single.html">Umcka Cold Care</a></h3>
-                <p class="price">$120.00</p>
-              </div>
+              
+<?php   
+
+
+$new_products = mysqli_query($con, "SELECT * FROM products WHERE quantity < 10 LIMIT 4");
+
+while ($row_new_products = mysqli_fetch_assoc($new_products)){
+
+?>
+
+
 
               <div class="text-center item mb-4">
-                <a href="shop-single.html"> <img src="images/product_01.png" alt="Image"></a>
-                <h3 class="text-dark"><a href="shop-single.html">Umcka Cold Care</a></h3>
-                <p class="price">$120.00</p>
+              <a href="single_product.php?PID=<?php echo $row_new_products['product_id'];  ?>"> <img src="../Adminsite/assets/img/product_images/<?php echo $row_new_products['image']   ?>" alt="Image" style="max-width: 200px;max-height: 200px;min-width: 200px; min-height: 200px;"></a>
+              <h3 class="text-dark"><a href="shop-single.html"><?php  echo $row_new_products['name'];  ?></a></h3>
+                <p class="price">Rs. <?php  echo $row_new_products['price'];  ?></p>
               </div>
 
-              <div class="text-center item mb-4">
-                <a href="shop-single.html"> <img src="images/product_02.png" alt="Image"></a>
-                <h3 class="text-dark"><a href="shop-single.html">Umcka Cold Care</a></h3>
-                <p class="price">$120.00</p>
-              </div>
-
-              <div class="text-center item mb-4">
-                <a href="shop-single.html"> <img src="images/product_04.png" alt="Image"></a>
-                <h3 class="text-dark"><a href="shop-single.html">Umckail Cold Care</a></h3>
-                <p class="price">$120.00</p>
-              </div>
+         <?php  }  ?>
 
             </div>
           </div>
